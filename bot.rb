2f5ea24ac415a0678eefeaf46e2b7c91b76e9958
Josh Friese <Friese@Friese.local>
2017-04-16 19:15:17 -0400

    built something

--- bot.rb
+++ bot.rb
@@ -7,16 +7,18 @@ while true
         # authenticate it for your account
         # fill in the following
         config = {
-            consumer_key:        '',
-            consumer_secret:     '',
-            access_token:        '',
-            access_token_secret: ''
+            consumer_key:        'mDPelYmcdf2LAWOkqUSifVcKG',
+            consumer_secret:     'qQpM55U36XmPXdaCrwtH92yxq9abBxm8eD9AaEq455pYkxpvjh',
+            access_token:        '853344652036120577-thGfdqlDIXEc4TWOkr3hFWjFgqRU5TZ',
+            access_token_secret: 'taahRUJQHNnD3Og2bQkh42gwzL6mNOugVmo4BWMBcGGZH'
         }
         rClient = Twitter::REST::Client.new config
         sClient = Twitter::Streaming::Client.new(config)
 
-        # topics to watch
-        topics = ['#duke sucks']  # add more, seperated by commas, to watch more topics
+        # rClient.update("Testing") #  uncomment and run this, then navigate to https://twitter.com/jftestbot
+
+        # set topic to watch
+        topics = ['#easter']  # add more, seperated by commas, to watch more topics
         sClient.filter(:track => topics.join(',')) do |tweet|
             if tweet.is_a?(Twitter::Tweet)
               puts tweet.text
